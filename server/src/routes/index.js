082493a602b31ea2d19f5df83cb158d5a084e48b
justin head <justin_head@ymail.com>
2018-06-03 19:22:42 -0500

    fixed backend of mailgun

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import jobRouter from './jobs';
+import contactRouter from './contactform';
+import stripeChargeRouter from './stripeCharge';
 
 let router = Router();
 
 
 
 router.use('/jobs', jobRouter);
+router.use('/donate', stripeChargeRouter);
+router.use('/contact', contactRouter);
 
 export default router;
